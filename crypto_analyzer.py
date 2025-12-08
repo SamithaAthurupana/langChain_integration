e9560b8b136c39c8342791874d23dbc5d4944258
SamithaAthurupana <athurupana1998@gmail.com>
2025-12-08 13:27:01 +0530

    commit with requirements

--- crypto_analyzer.py
+++ crypto_analyzer.py
@@ -9,7 +9,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from starlette import status
 
-from models import CryptoAnalysisResponse
+from models import CryptoAnalysisResponse, CryptoRequest
 
 load_dotenv()
 
@@ -19,7 +19,7 @@ COINGECKO_URL = os.getenv("COINGECKO_URL")
 
 app = FastAPI(description="AI Powered Crypto Analysis API")
 
-def get_crypto_data(coin_list: List[str]):
+def get_crypto_data(coin_list: List[str])->CryptoAnalysisResponse:
     if not coin_list:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No coins provided")
 
@@ -32,26 +32,31 @@ def get_crypto_data(coin_list: List[str]):
     crypto_api_resp = requests.get(COINGECKO_URL, params=params)
     return crypto_api_resp.json()
 
-chat_prompt = ChatPromptTemplate.from_template(
-    """
-    You are a "CryptoAnalyst - AI" a professional cryptocurrency market analyst
+
+def get_crypto_analysis_response(coins : List[str]):
+
+    chat_prompt = ChatPromptTemplate.from_template(
+        """
+        You are a "CryptoAnalyst - AI" a professional cryptocurrency market analyst
+        
+        You will be given recent market data for multiple cryptocurrencies (price, market cap, volume, 24h change)
+        
+        Here is a market Data:
+        {market_data}
     
-    You will be given recent market data for multiple cryptocurrencies (price, market cap, volume, 24h change)
-    
-    Here is a market Data:
-    {market_data}
+    Rules:
+        - Return one analysis per cryptocurrency
+        - Provide 3 key_factors and 3 insights per coin
+        - Base your reasoning on given metrics (e.g price change %, market cap trend)
+        """
+    )
+    llm = ChatOpenAI(model="meta-llama/llama-3.3-70b-instruct:free",
+                     base_url=OPENROUTER_URL, api_key=OPENROUTER_KEY).with_structured_output(CryptoAnalysisResponse)
+    responses_chain = chat_prompt | llm  # LCEL = langchain expression language
+    resp = responses_chain.invoke({"market_data": json.dumps(get_crypto_data(coins))})
+    return resp
 
-Rules:
-    - Return one analysis per cryptocurrency
-    - Provide 3 key_factors and 3 insights per coin
-    - Base your reasoning on given metrics (e.g price change %, market cap trend)
-    """
-)
-llm = ChatOpenAI(model="meta-llama/llama-3.3-70b-instruct:free",
-                      base_url=OPENROUTER_URL, api_key=OPENROUTER_KEY).with_structured_output(CryptoAnalysisResponse)
+@app.post("/crypto/analysis")
+def analyze_crypto(request : CryptoRequest):
 
-responses_chain = chat_prompt | llm #LCEL = langchain expression language
-
-resp = responses_chain.invoke({"market_data" : json.dumps(get_crypto_data(["bitcoin", "ethereum"]))})
-
-print(resp)
+    return get_crypto_analysis_response(request.coins)
